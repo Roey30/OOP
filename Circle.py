6a801c1eefb2cea4efb520c60962e9aa175caaf0
Roey30 <113986181+Roey30@users.noreply.github.com>
2022-10-01 18:25:47 +0300

    Update Circle.py

--- Circle.py
+++ Circle.py
@@ -1,5 +1,6 @@
 # IMPORTS
 from Shape import *
+import math
 
 
 class Circle(Shape):
@@ -19,17 +20,25 @@ class Circle(Shape):
         calculates the area of self(circle)
         :return: the area of self
         """
-        return (self.radios ** 2) * 3.14
+        return (self.radios ** 2) * math.pi
 
     def Perimeter_Calculation(self):
         """
         calculates the perimeter of self(circle)
         :return: the area of self
         """
-        return (self.radios * 2) * 3.14
+        return (self.radios * 2) * math.pi
 
     def Get_Radios(self):
         """
         :return: the radios of self(circle)
         """
         return self.radios
+
+
+if __name__ == '__main__':
+    circle = Circle("Yellow", 78.539, 31.415, 5)
+    assert circle.Get_Colour() == "Yellow"
+    assert circle.Get_Area() == 78.539
+    assert circle.Get_Perimeter() == 31.415
+    assert circle.Get_Radios() == 5
