2df313e32d2c1fc6b65f1dcf009711099572557a
Roey30 <113986181+Roey30@users.noreply.github.com>
2022-11-18 13:41:43 +0200

    Add files via upload

---
+++ DataBase/Database.py
@@ -0,0 +1,16 @@
+import os
+
+
+
+class Database:
+    def __int__(self, dictionary):
+        self.dictionary = dictionary
+
+    def value_set(self, key, val):
+
+
+    def get_value(self, key):
+
+
+    def delete_value(self, key):
+
